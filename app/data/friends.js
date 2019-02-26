a594237203ed182f2cca35c3f1b03d70021c1f05
Reese Fairbanks <reese@reeses-mbp.attlocal.net>
2019-02-26 10:18:35 -0500

    Added survey and html routes

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,11 @@
+//Router, points server to the right route files
+require("./routing/apiRoutes")(app);
+require("./routing/htmlRoutes")(app);
 
+var friendsList = 
 
 [{
-    "name":"Ahmed",
+    "name":"Joe",
     "photo":"",
     "scores":[
         5,
@@ -17,18 +21,36 @@
       ]
   },
   {
-    "name":"Ahmed",
+    "name":"Jane",
     "photo":"",
     "scores":[
-        5,
-        1,
         4,
-        4,
-        5,
-        1,
         2,
-        5,
+        3,
+        3,
         4,
-        1
+        2,
+        1,
+        4,
+        3,
+        2
       ]
-}]
+},
+{
+    "name":"Mary",
+    "photo":"",
+    "scores":[
+        3,
+        5,
+        2,
+        2,
+        3,
+        5,
+        5,
+        3,
+        2,
+        5
+      ]
+}];
+
+module.exports = friendsList;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,5 @@
+var friendList = require("../data/friends.js");
+
+module.exports = function(app) {
+    app.get("/api/friends", function(req, res))
+}
